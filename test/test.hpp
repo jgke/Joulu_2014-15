f4a0a9c7f4a83c1087f3694a12118499421c3fb3
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2014-12-20 22:57:44 +0200

    add documentation to test.hpp

--- test/test.hpp
+++ test/test.hpp
@@ -1,3 +1,9 @@
+/**
+ * \file test.hpp
+ * Test framework.
+ */
+
+
 #ifndef TEST_HPP
 #define TEST_HPP
 
@@ -5,13 +11,44 @@
 #include <string>
 #include <sstream>
 
+/**
+ * Initialize tests.
+ */
 void init_tests();
+/**
+ * Print a successful test.
+ * @param name name of the test
+ */
 void test_success(const char *name);
+/**
+ * Print a failed test.
+ * @param name name of the test
+ */
 void test_failure(const char *name);
+/**
+ * Print a failed test, with a reason.
+ * @param name name of the test
+ * @param reason reason why the test failed
+ */
 void test_failure(const char *name, const char *reason);
+/**
+ * Test that is the value true.
+ * @param name name of the test
+ * @param value boolean to test
+ */
 void test_true(const char *name, bool value);
+/**
+ * Test that is the value false.
+ * @param name name of the test
+ * @param value boolean to test
+ */
 void test_false(const char *name, bool value);
-void print_test_status();
+/**
+ * Test that are two values equal.
+ * @param name name of the test
+ * @param a expected value
+ * @param b value to test
+ */
 template <class T> void test_equal(const char *name, T a, T b) {
     if(a == b)
         test_success(name);
@@ -25,6 +62,11 @@ template <class T> void test_equal(const char *name, T a, T b) {
     }
 }
 
+/**
+ * Test that does the piece of code run without throwing an exception.
+ * @param name name of the test
+ * @param func code to run
+ */
 #define NO_DEATH_TEST(name, func) \
     do { \
         try { \
@@ -35,6 +77,11 @@ template <class T> void test_equal(const char *name, T a, T b) {
         } \
     } while(0) 
 
+/**
+ * Test that does the piece of code throw an exception.
+ * @param name name of the test
+ * @param func code to run
+ */
 #define DEATH_TEST(name, func) \
     do { \
         try { \
@@ -44,5 +91,10 @@ template <class T> void test_equal(const char *name, T a, T b) {
             test_success(name); \
         } \
     } while(0)
+
+/**
+ * Print status of the tests.
+ */
+void print_test_status();
 
 #endif
