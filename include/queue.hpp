5c42447bd9de231d12158ba7fd62eacf7c813ff2
Jaakko Hannikainen <jaakko.hannikainen@paivola.fi>
2014-12-26 01:00:26 +0200

    size support for queue

--- include/queue.hpp
+++ include/queue.hpp
@@ -46,12 +46,17 @@ template <class T> class Queue {
          */
         const T &pop();
         /**
+         * Returns the amount of members in this queue.
+         */
+        int size();
+        /**
          * Destructor for queue.
          */
         ~Queue();
     private:
        QueueNode<T> *root; 
        QueueNode<T> *tail; 
+       int membercount;
 };
 
 /**
@@ -78,11 +83,13 @@ template <class T> class QueueNode {
 };
 
 template <class T> Queue<T>::Queue() {
+    this->membercount = 0;
     this->root = NULL;
     this->tail = NULL;
 }
 
 template <class T> Queue<T>::Queue(const Queue<T> &queue) {
+    this->membercount = 0;
     this->root = NULL;
     this->tail = NULL;
     QueueNode<T> *cur = queue.root;
@@ -93,6 +100,7 @@ template <class T> Queue<T>::Queue(const Queue<T> &queue) {
 }
 
 template <class T> Queue<T> &Queue<T>::operator=(const Queue<T> &queue) {
+    this->membercount = 0;
     this->root = NULL;
     this->tail = NULL;
     QueueNode<T> *cur = queue.root;
@@ -104,6 +112,7 @@ template <class T> Queue<T> &Queue<T>::operator=(const Queue<T> &queue) {
 }
 
 template <class T> void Queue<T>::add(const T &value) {
+    this->membercount++;
     QueueNode<T> *newtail = new QueueNode<T>(this->tail, value);
     if(tail != NULL)
         tail->next = newtail;
@@ -121,6 +130,7 @@ template <class T> const T &Queue<T>::peek() const {
 }
 
 template <class T> const T &Queue<T>::pop() {
+    this->membercount--;
     QueueNode<T> *retnode = root;
     root = root->next;
     if(root == NULL)
@@ -129,6 +139,10 @@ template <class T> const T &Queue<T>::pop() {
     delete retnode;
     return retvalue;
     
+}
+
+template <class T> int Queue<T>::size() {
+    return this->membercount;
 }
 
 template <class T> Queue<T>::~Queue() {
